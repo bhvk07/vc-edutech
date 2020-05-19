05487f6c60f5ac3dfd67682925ac3fbc0454871b
bhavik <bhavik@LAPTOP-KCQG0S6R>
2020-05-19 21:29:34 +0530

    academic year front end done

---
+++ src/main/webapp/javascript/aca-date.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$('#aca_start').on('click', function(){
+		$('.form_date1').datetimepicker({
+			format: 'd/m/Y', //date format
+			timepicker: false,
+			//dateFormat : 'YYYY-MM-DD',
+			weekStart : 1,
+			todayBtn : 1,
+			autoclose : true,
+			todayHighlight : 1,
+			startView : 2,
+			minView : 2,
+			forceParse : 0,
+			 
+
+		});
+		/*$(".form_date").datetimepicker();*/
+	});
+	$('#aca_end').on('click', function(){
+		$('.form_date2').datetimepicker({
+			format: 'd/m/Y', //date format
+			timepicker: false,
+			//dateFormat : 'YYYY-MM-DD',
+			weekStart : 1,
+			todayBtn : 1,
+			autoclose : 1,
+			todayHighlight : 1,
+			startView : 2,
+			minView : 2,
+			forceParse : 0,
+			
+
+		});
+		/*$(".form_date").datetimepicker();*/
+	});
+	});
