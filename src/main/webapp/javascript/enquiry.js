c15b2bff8314e1326b7bab7eb6a8f2147a31695c
Shewale <shewaledivyakant@gmail.com>
2020-05-24 19:56:35 +0530

    trying to pergorm frontend validation but not working....

--- src/main/webapp/javascript/enquiry.js
+++ src/main/webapp/javascript/enquiry.js
@@ -1,15 +1,38 @@
 var mes;
 
 $(document).ready(function() {
+	$('form[id="EnquiryForm"]').validate({
+		  rules: {
+		    fname: {
+		    	required:true
+		    },
+		    lname: {
+		      required: true
+		    }
+		  },
+		  messages: {
+			sname: {
+				required:'This field is required'
+			},
+			mname: {
+				required:'This field is required'
+		    }
+		  },
+		  submitHandler:function(form){
+			  event.preventDefault();
+			  EnquiryData();
+		  }
+	});
+
 	FetchAllEmployee();
 	//getCurrentDate();
-	$("#enq_data_submit").click(function() {
+	/*$("#enq_data_submit").click(function() {
 		var token=sessionStorage.getItem("token");
 		validateLogin(token);
 		 
 		event.preventDefault();
 		EnquiryData();
-	});
+	});*/
 	$("#Add_employee").click(function() {
 		event.preventDefault();
 		AddEmployee();
