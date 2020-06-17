51400c92c326c8a6ae323fb1c58d80a4ec7c9027
bhavik <bhavik@LAPTOP-KCQG0S6R>
2020-06-17 14:59:50 +0530

    Time Tabe report design done. (Use accordian_view.js) for any accordian content.

---
+++ src/main/webapp/javascript/accordian_view.js
@@ -0,0 +1,34 @@
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		var links = this.el.find('.content-entry .accordian_title');
+		links.on('click', {
+			el : this.el,
+			multiple : this.multiple
+		}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+		/* alert(e.data); */
+		$this = $(this), $next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.accordion-content').not($next).slideUp().parent()
+					.removeClass('open');
+		}
+		;
+	}
+	var accordion = new Accordion($('.accordion-container'), false);
+});
+
+$('.accordian_title').on('click', function(event) {
+	if (!$(event.target).closest('#accordion').length) {
+		$this.parent().toggleClass('open');
+	}
+});
