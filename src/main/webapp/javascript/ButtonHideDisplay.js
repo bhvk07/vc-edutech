0e2f4d986d8b43fb76d7e6fb2990909f30674e55
ACER <ACER@LAPTOP-OH4DA7B9>
2020-06-24 21:24:53 +0530

    styling on Subject and Caste.html

--- src/main/webapp/javascript/ButtonHideDisplay.js
+++ src/main/webapp/javascript/ButtonHideDisplay.js
@@ -2,33 +2,30 @@
 					        var chk = 'input[id="checkbox"]';
 					        var $edit = $("#editBtn").hide();
 					        var $del = $("#deleteBtn").hide();
+					        var $view = $("#btn-view").hide();
 					        
-					      
-					          
-					          $(document).on('change','#feestypetable input:checkbox',function () {
-						  		    if($('#feestypetable input:checkbox:checked').length > 1 ){
+
+					          $(document).on('change','.TableClass input:checkbox',function () {
+						  		    if($('.TableClass input:checkbox:checked').length > 1 ){
 						  		    	$('#editBtn').hide();
+						  		    	$('#btn-view').hide();
 						  		    	 $('#deleteBtn').show();
 						  		    	
 						  		    }
-						  		    else  if($('#feestypetable input:checkbox:checked').length == 0) {
+						  		    else  if($('.TableClass input:checkbox:checked').length == 0) {
 						  		    
 						  		    	$('#deleteBtn').hide();
 						  		    	$('#editBtn').hide();
+						  		    	$('#btn-view').hide();
 						  		    	
 						  		    }
 						  		    else  {
 						  		    	$('#editBtn').show();
 						  		    	 $('#deleteBtn').show();
+						  		    	$('#btn-view').show();
 						  		    	
 						  		    }
-
-						  		   
+				  		   
 						  		});
-					          
-					          
-					   
-					  		
-					  		
-					  		
+
 					      });
